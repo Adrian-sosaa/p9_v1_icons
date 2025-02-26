7a524beb65e9cfd3055a52dc79f8b9a2c79c3af9
JAN_ADRIAN_SOSA_CORDERO <a.22308051281317@cbtis128.edu.mx>
2025-02-26 20:10:51 +0000

    Jan sosa

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Jan Sosa"),
+            titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            centerTitle: true,
+            backgroundColor: Colors.blue,
+          ),
+          body: const Column(
+            children: <Widget>[
+              Text("Jan adrian sosa 22308051281317",
+                  style: TextStyle(fontSize: 40, color: Color(0xff2ab1ef))),
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(
+                    Icons.airport_shuttle,
+                    color: Colors.indigo,
+                    size: 40.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.audiotrack,
+                    color: Colors.pinkAccent,
+                    size: 40.0,
+                  ),
+                  Icon(
+                    Icons.add_home_rounded,
+                    color: Colors.blueAccent,
+                    size: 40.0,
+                  ),
+                  Icon(
+                    Icons.adb,
+                    color: Colors.lime,
+                    size: 40.0,
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
